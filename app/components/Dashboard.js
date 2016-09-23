d5df69c5f94b5b2cbd2c27ff67111c48c520f9ae
Ricardo Hinds <hindsricardo@gmail.com>
2016-09-23 09:28:13 -0500

    TabBar added

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,61 +1,49 @@
-/* app/components/Dashboard.js */
+/* application/components/Dashboard.js */
+import React, { Component } from 'react';
+import { TabBarIOS } from 'react-native';
+import { TabBarItemIOS } from 'react-native-vector-icons/Ionicons';
 
-import React, {Component} from 'react';
-import {
-	View,
-	Text,
-	TouchableOpacity
-} from 'react-native';
+import ActivityView from './activity/ActivityView';
+import MessagesView from './messages/MessagesView';
+import ProfileView from './profile/ProfileView';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
-import Colors from '../styles/colors';
-import { globals } from '../styles';
-
-const BackButton = ({handlePress}) => (
-	<TouchableOpacity onPress={handlePress} style={globals.pal}>
-		<Icon name='ios-arrow-back' size={25} color='white' />
-	</TouchableOpacity>
-);
-
-class Dashboard extends Component {
-	constructor(){
-		super();
-		this.goBack = this.goBack.bind(this);
-		this.visitLanding = this.visitLanding.bind(this);
-	}
-
-	visitLanding(){
-		this.props.navigator.push(
-			name: 'Landing'
-		);
-	}
-
-	goBack(){
-		this.props.navigator.pop();
-	}
-
-	render(){
-		let titleConfig = { title: 'Dashboard', tintColor: 'white'};
-		return(
-			<View style={globals.flexContainer}>
-				<NavigationBar
-					title={titleConfig}
-					tintColor={Colors.brandPrimary}
-					leftButton={<BackButton handlePress={this.goBack}/>} />
-				<View style={globals.flexCenter}>
-		          <Text style={globals.h2}>
-		            This is the Dashbaord
-		          </Text>
-		          <TouchableOpacity onPress={this.visitLanding}>
-		            <Text>
-		              Go to the Landing Page
-		            </Text>
-		          </TouchableOpacity>
-		        </View>
-		      </View>
-		)
-	}
+class Dashboard extends Component{
+  constructor(){
+    super();
+    this.state = {
+      selectedTab: 'Activity'
+    }
+  }
+  render(){
+    return (
+      <TabBarIOS>
+        <TabBarItemIOS
+          title='Activity'
+          selected={this.state.selectedTab === 'Activity'}
+          iconName='ios-pulse'
+          onPress={() => this.setState({ selectedTab: 'Activity' })}
+        >
+          <ActivityView />
+        </TabBarItemIOS>
+        <TabBarItemIOS
+          title='Messages'
+          selected={this.state.selectedTab === 'Messages'}
+          iconName='ios-chatboxes'
+          onPress={() => this.setState({ selectedTab: 'Messages' })}
+        >
+          <MessagesView />
+        </TabBarItemIOS>
+        <TabBarItemIOS
+          title='Profile'
+          selected={this.state.selectedTab === 'Profile'}
+          iconName='ios-person'
+          onPress={() => this.setState({ selectedTab: 'Profile' })}
+        >
+          <ProfileView />
+        </TabBarItemIOS>
+      </TabBarIOS>
+    )
+  }
 }
 
 export default Dashboard;
